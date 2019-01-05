11282fc8ab7659589209de8915f01ed011637237
leon685 <32682417+leon685@users.noreply.github.com>
2019-01-05 15:42:28 +0100

    threadfix

--- JumpyRushyProjekt/Assets/Script/LoadingFinished.cs
+++ JumpyRushyProjekt/Assets/Script/LoadingFinished.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadingFinished : MonoBehaviour {
-    bool load = false;
 	// Use this for initialization
 	void Start () {
-        Thread nit = new Thread(new ThreadStart(check));
-        nit.Start();
-    }
-    void check()
-    {
-        while (!CompressAll.loadingDone)
-        {
-
-        }
-        load = true;
+        
     }
     // Update is called once per frame
     void Update () {
-		if (load)
+        //Thread nit = new Thread(new ThreadStart(check));
+        //nit.Start();
+
+        if (CompressAll.loadingDone)
         {
             SceneManager.LoadScene("Menu");
         }
